60c62490f9f715e977b63471761a1bda6627522a
pablodarocha <pablodarocha.ribeiro@gmail.com>
2016-10-26 23:32:30 -0200

    + criar tarefas

--- www/js/app.js
+++ www/js/app.js
@@ -24,3 +24,35 @@ angular.module('starter', ['ionic'])
 })
 
 angular.module('todo', ['ionic'])
+
+  .controller('TodoCtrl', function ($scope, $ionicModal) {
+    // no need for testing data anymore
+    $scope.tasks = [];
+
+    // create and load the modal
+    $ionicModal.fromTemplateUrl('new-task.html', function (modal) {
+      $scope.taskModal = modal;
+    }, {
+      scope: $scope,
+      animation: 'slide-in-up'
+    });
+
+    // called when the form is submitted
+    $scope.createTask = function (task) {
+      $scope.tasks.push({
+        title: task.title
+      });
+      $scope.taskModal.hide();
+      task.title = "";
+    };
+
+    // open our new task modal
+    $scope.newTask = function () {
+      $scope.taskModal.show();
+    };
+
+    // close the new task modal
+    $scope.closeNewTask = function () {
+      $scope.taskModal.hide();
+    };
+})
